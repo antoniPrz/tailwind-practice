ba634f0cbbf8b232816cc5cb7124a2e832734c92
Dev7 <dev7@MacBook-Pro-de-Dev7.local>
2023-08-07 09:30:48 -0400

    taiwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Ruta y extención de archivos donde teilwind sera aplicado 
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    // Por defecto viene asi:
+    // './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    // './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    //---------------------------------------//
+
+    // Aplicaremos solo para archivos typescript dentro de src
+    './src/**/*.tsx',
   ],
   theme: {
+    // extender el tema de tailwind, incluir estilos personalizados
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+
+        colors:{mandarina:'#DE6749'}
+
+
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
     },
   },
   plugins: [],
