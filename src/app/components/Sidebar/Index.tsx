5e6ec14a91509d09a8f0cdc414c73aeb08bb3af7
Dev7 <dev7@MacBook-Pro-de-Dev7.local>
2023-08-09 22:04:52 -0400

    NavItems

--- src/app/components/Sidebar/Index.tsx
+++ src/app/components/Sidebar/Index.tsx
@@ -1,5 +1,6 @@
 import { Logo } from "./Logo";
 import {Search} from "lucide-react"
+import { MainNavigation } from "./MainNavegation";
 
 
 export function Sidebar(){
@@ -8,10 +9,11 @@ export function Sidebar(){
       <aside className=' border-r border-zinc-200 px-5 py-8 space-y-6'>
       <Logo/>
       {/* searchbar */}
-      <div className="border border-zinc-300 px-3 py-2 gap-2 rounded-lg w-full flex items-center shadow-md">
-        <Search className="h-5 w-5 text-zinc-500"/>
+      <div className=" mx-1 border border-zinc-300 px-3 py-2 gap-2 rounded-lg w-full flex items-center shadow-md">
+        <Search className="mx-1h-5 w-5 text-zinc-500"/>
         <input type="text" className="flex-1 p-0 text-zinc-900 placeholder-zinc-600 border-0 bg-transparent" placeholder="Search"/>
       </div>
+      <MainNavigation/>
 
 
       </aside>
