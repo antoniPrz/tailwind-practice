e184908938c42099130b7350c513effc0d240e5c
Dev7 <dev7@MacBook-Pro-de-Dev7.local>
2023-08-10 07:39:14 -0400

    saved to make a new branch

--- src/app/components/Sidebar/Index.tsx
+++ src/app/components/Sidebar/Index.tsx
@@ -1,22 +1,30 @@
 import { Logo } from "./Logo";
-import {Search} from "lucide-react"
+import { Search } from "lucide-react";
 import { MainNavigation } from "./MainNavegation";
 
+export function Sidebar() {
+  return (
+    <>
+      <aside className=" bg-yellow-400 border-r border-zinc-200 px-5 py-8 flex flex-col gap-6">
+        <Logo />
+        {/* searchbar */}
+        <div className=" mx-1 border border-zinc-300 px-3 py-2 gap-2 rounded-lg w-full flex items-center shadow-md">
+          <Search className="mx-1h-5 w-5 text-zinc-500" />
+          <input
+            type="text"
+            className="flex-1 p-0 text-zinc-900 placeholder-zinc-600 border-0 bg-transparent"
+            placeholder="Search"
+          />
+        </div>
 
-export function Sidebar(){
-    return (<>
-         
-      <aside className=' border-r border-zinc-200 px-5 py-8 space-y-6'>
-      <Logo/>
-      {/* searchbar */}
-      <div className=" mx-1 border border-zinc-300 px-3 py-2 gap-2 rounded-lg w-full flex items-center shadow-md">
-        <Search className="mx-1h-5 w-5 text-zinc-500"/>
-        <input type="text" className="flex-1 p-0 text-zinc-900 placeholder-zinc-600 border-0 bg-transparent" placeholder="Search"/>
-      </div>
-      <MainNavigation/>
+        <MainNavigation />
 
-
+        <div className="mt-auto bg-sky-500 flex flex-col gap-6 ">
+          <div className="space-y-0.5">
+          new section
+          </div>
+        </div>
       </aside>
-
-    </>)
+    </>
+  );
 }
